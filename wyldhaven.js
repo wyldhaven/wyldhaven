e74ebef0038e593a258ff6b8263ab01c8e2c495c
wyldhaven <40779715+wyldhaven@users.noreply.github.com>
2018-07-02 20:20:23 -0500

    Add files via upload

---
+++ wyldhaven.js
@@ -0,0 +1,53 @@
+var WYLD = (function($, WYLD) {
+
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function() {
+
+            });
+        }
+    });
+});
+
+
+
+var setupValues = function() {
+  scrollArrow = document.querySelector('.arrow-container');
+  scrollCopy = document.querySelector('.scroll');
+};
+
+var animateIntro = function() {
+  $(".scroll").delay(3500).animate({
+        opacity: .6
+    }, 3000, function() {
+        $(".arrow-container").animate({
+            opacity: .6
+        }, 1500);
+    });
+
+};
+
+
+var init = function() {
+  setupValues();
+  animateIntro();
+};
+
+
+
+WYLD.scrolling = {
+        init: init
+    };
+
+    return WYLD;
+
+}($, WYLD || {}));
