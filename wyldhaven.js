e10ad990865165c3640ef798e1f9bc7205255dec
wyldhaven <40779715+wyldhaven@users.noreply.github.com>
2019-08-21 20:41:36 -0400

    Update wyldhaven.js

--- wyldhaven.js
+++ wyldhaven.js
@@ -64,11 +64,11 @@ function erase(elem) {
   var test = elem[0].style.opacity;
   if ($(window).width() >= 680) {
     if (test > 0) {
-      elem.css('opacity', (counter -= 0.04));
+      elem.css('opacity', (counter -= 0.03));
     }
   } else {
       if (test > 0) {
-        elem.css('opacity', (counter -= 0.05));
+        elem.css('opacity', (counter -= 0.03));
       }
     }
   }
