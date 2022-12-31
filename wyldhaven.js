e8194ade73421dc1aa7635f74b25f0c85d0433fa
wyldhaven <40779715+wyldhaven@users.noreply.github.com>
2022-12-30 19:38:05 -0500

    Update wyldhaven.js

--- wyldhaven.js
+++ wyldhaven.js
@@ -14,27 +14,27 @@ $(document).ready(function() {
     });
 });
 
-var setupValues = function() {
-  scrollArrow = document.querySelector('.arrow-container');
-  scrollCopy = document.querySelector('.scroll');
-};
+// var setupValues = function() {
+//   scrollArrow = document.querySelector('.arrow-container');
+//   scrollCopy = document.querySelector('.scroll');
+// };
 
-var animateIntro = function() {
-  $(".scroll").delay(1900).animate({
-        opacity: .25
-    }, 2300, function() {
-        $(".arrow-container").animate({
-            opacity: .35
-        }, 1500);
-    });
+// var animateIntro = function() {
+//   $(".scroll").delay(1900).animate({
+//         opacity: .25
+//     }, 2300, function() {
+//         $(".arrow-container").animate({
+//             opacity: .35
+//         }, 1500);
+//     });
 
-};
+// };
 
 
-var init = function() {
-  setupValues();
-  animateIntro();
-};
+// var init = function() {
+//   setupValues();
+//   animateIntro();
+// };
 
 WYLD.scrolling = {
   init: init
@@ -44,51 +44,51 @@ WYLD.scrolling = {
 
 }($, WYLD || {}));
 
-let counter = 0;
-function show(elem) {
-  var test = elem[0].style.opacity;
-  if ($(window).width() >= 680) {
-    if (test < 1) {
-      elem.css('opacity', (counter += 0.03));
-    }
-  } else {
-    if (test < 1) {
-      elem.css('opacity', (counter += 0.06));
-    }
-  }
-}
+// let counter = 0;
+// function show(elem) {
+//   var test = elem[0].style.opacity;
+//   if ($(window).width() >= 680) {
+//     if (test < 1) {
+//       elem.css('opacity', (counter += 0.03));
+//     }
+//   } else {
+//     if (test < 1) {
+//       elem.css('opacity', (counter += 0.06));
+//     }
+//   }
+// }
 
-function erase(elem) {
-  var test = elem[0].style.opacity;
-  if ($(window).width() >= 680) {
-    if (test > 0) {
-      elem.css('opacity', (counter -= 0.04));
-    }
-  } else {
-      if (test > 0) {
-        elem.css('opacity', (counter -= 0.03));
-      }
-    }
-  }
+// function erase(elem) {
+//   var test = elem[0].style.opacity;
+//   if ($(window).width() >= 680) {
+//     if (test > 0) {
+//       elem.css('opacity', (counter -= 0.04));
+//     }
+//   } else {
+//       if (test > 0) {
+//         elem.css('opacity', (counter -= 0.03));
+//       }
+//     }
+//   }
 
-var position = $(window).scrollTop();
-$(window).scroll(function() {
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
-  var elemToppy = $('h3').offset().top;
-  var elemBottom = elemToppy + $('h3').height();
+// var position = $(window).scrollTop();
+// $(window).scroll(function() {
+//   var docViewTop = $(window).scrollTop();
+//   var docViewBottom = docViewTop + $(window).height();
+//   var elemToppy = $('h3').offset().top;
+//   var elemBottom = elemToppy + $('h3').height();
 
-  var scroll = $(window).scrollTop();
-  if (docViewBottom >= (Number(elemBottom) - 80) && docViewTop <= (Number(elemBottom)) - 70) {
-    if (docViewTop >= (Number(elemBottom)) - 400) {
-        erase($('h3'));
-    } else {
-        if (scroll > position) {
-           show($('h3'));
-        } else {
-           erase($('h3'));
-        }
-      }
-    }
-  position = scroll;
-});
+//   var scroll = $(window).scrollTop();
+//   if (docViewBottom >= (Number(elemBottom) - 80) && docViewTop <= (Number(elemBottom)) - 70) {
+//     if (docViewTop >= (Number(elemBottom)) - 400) {
+//         erase($('h3'));
+//     } else {
+//         if (scroll > position) {
+//            show($('h3'));
+//         } else {
+//            erase($('h3'));
+//         }
+//       }
+//     }
+//   position = scroll;
+// });
